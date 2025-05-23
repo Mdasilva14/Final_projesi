e9437f8356201debb04ab0840835c03167aa9dc3
MARİO ENRİQUE MOTEDE DASİLVA <mariomotede69@gmail.com>
2025-05-24 00:30:22 +0300

    Update app.py

--- final_hafta/app.py
+++ final_hafta/app.py
@@ -163,8 +163,10 @@ def visitors_list():
     return render_template('visitors.html', visitors=visitors)
 
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+#if __name__ == '__main__':
+    #with app.app_context():
+        #db.create_all()
+    #app.run(debug=True)
 
+import os
+app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
